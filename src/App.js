3aabc238ee1019125c74dcc7fa7c2d9a0b34f44a
Elena <eli92pavleska@gmail.com>
2018-12-13 16:00:58 +0100

    git ignore updated and HashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import Contacts from './components/contacts/Contacts';
 import Header from './components/layout/Header';
 import About from './components/pages/About';
@@ -18,6 +18,7 @@ class App extends Component {
     return (
       <Provider>
           <Router>
+          {/*<Router basename={process.env.PUBLIC_URL}>*/}
             <div className="App">
               <Header branding="Contact Manager" />
               <div className="container">
